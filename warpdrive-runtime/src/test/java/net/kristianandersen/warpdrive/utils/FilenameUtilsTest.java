4e3e567107b784d362d65c4cb1b12b7b7ed10288
Kristian Andersen <kriand@gmail.com>
2010-03-31 00:51:48 +0200

    Removed

---
+++ warpdrive-runtime/src/test/java/net/kristianandersen/warpdrive/utils/FilenameUtilsTest.java
@@ -0,0 +1,19 @@
+package net.kristianandersen.warpdrive.utils;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kriand
+ * Date: Mar 30, 2010
+ * Time: 8:14:24 PM
+ */
+public class FilenameUtilsTest {
+
+    @Test
+    public void testAppendVersion() {
+
+    }
+    
+
+}
